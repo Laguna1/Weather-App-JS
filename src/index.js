e909eed2aec0524f478b8fac6c6a9a82c8a827ba
Laguna1 <lagunazoomarket@gmail.com>
2020-12-10 01:38:42 +0200

    Add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import "./style.css";
+import "bootstrap";
+import "./scss/app.scss";
 
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   // element.innerText = "Hello webpack";
-  element.innerHTML = "<h1>Hello</h1>";
+  element.innerHTML = "<h1>Weather App</h1>";
   element.classList.add("hello");
 
   return element;
 }
 
 document.body.appendChild(component());
+
+// 1.function for location
+
+const setLocation = () => {};
