7fbb51ef1824541e515a09e024ce26f60a3d439e
Laguna1 <lagunazoomarket@gmail.com>
2020-12-11 01:07:40 +0200

    Add function for location

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
-import "./style.css";
-import "bootstrap";
-import "./scss/app.scss";
-
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   // element.innerText = "Hello webpack";
   element.innerHTML = "<h1>Weather App</h1>";
-  element.classList.add("hello");
+  element.classList.add("text-center");
 
   return element;
 }
@@ -17,4 +13,14 @@ document.body.appendChild(component());
 
 // 1.function for location
 
-const setLocation = () => {};
+// const setLocation = () => {};
+// 5591105db82a0e4b34df846e33599b1a
+//  http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1111111111
+function fetchCityData(cityName) {
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=5591105db82a0e4b34df846e33599b1a`,
+    { mode: "cors" }
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
