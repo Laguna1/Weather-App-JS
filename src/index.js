5fc9d48c32c7fb99a072fba254f89aebf042be6d
Laguna1 <lagunazoomarket@gmail.com>
2020-12-11 17:27:22 +0200

    Fix bootstrap setting and add html-form

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import "bootstrap";
+import "./scss/app.scss";
+import "./css/style.css";
+
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
   // element.innerText = "Hello webpack";
   element.innerHTML = "<h1>Weather App</h1>";
-  element.classList.add("text-center");
+  element.classList.add("text-center", "text-warning", "font-weight-bold");
 
   return element;
 }
@@ -16,11 +20,13 @@ document.body.appendChild(component());
 // const setLocation = () => {};
 // 5591105db82a0e4b34df846e33599b1a
 //  http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=1111111111
-function fetchCityData(cityName) {
+
+function cityData(cityName) {
   return fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=5591105db82a0e4b34df846e33599b1a`,
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=5591105db82a0e4b34df846e33599b1a`,
     { mode: "cors" }
   )
     .then((response) => response.json())
     .then((data) => console.log(data));
 }
+cityData("London");
